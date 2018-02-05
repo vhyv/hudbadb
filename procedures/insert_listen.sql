9d384fac381e4bc3b7bd134f78b37f44e9ccf54c
Vojtěch Hyvnar <vhyvnar@gmail.com>
2018-02-05 16:18:08 +0100

    Add files via upload

--- procedures/insert_listen.sql
+++ procedures/insert_listen.sql
@@ -28,13 +28,10 @@ BEGIN
 	DECLARE @artistid	int
 	DECLARE @albumid	int
 	DECLARE @datum		date
+	DECLARE @oldrating	decimal(3,1)
 	DECLARE @updatedrating	nvarchar(100)
 	DECLARE @insertedstuff	nvarchar(200)
-
-	SET @updatedrating = CONCAT('The rating was updated to ', CONVERT(nvarchar(50), @rating), '.')
-
-	SET @insertedstuff = CONCAT('The listening data of ', @albumname, ' by ', @artistname, ' was inserted. The rating is ', @rating, '/10.')
-
+	
 	IF @rating > 10.0 OR @rating < 0.0 
 	BEGIN
 		RAISERROR ('Rating must be between zero and ten.', 0, 1)
@@ -52,6 +49,33 @@ BEGIN
 					WHERE art_id = @artistid
 					AND alb_name = @albumname)
 
+	SET @oldrating = (SELECT rating
+						FROM listening_history
+						WHERE alb_id = @albumid)
+
+	SET @updatedrating = CONCAT('The rating was updated to ', CONVERT(nvarchar(50), @rating), '/10 from ', @oldrating, '/10.')
+
+	SET @insertedstuff = CONCAT('The listening data of ', @albumname, ' by ', @artistname, ' was inserted. The rating is ', @rating, '/10.')
+
+-- Pokud uz to tam je, updatuju jenom rating, ne favs nebo listening date. 
+
+	IF (EXISTS
+			(SELECT alb_id
+			FROM listening_history
+			WHERE alb_id = @albumid
+			AND rating IS NOT NULL AND ldate IS NOT NULL)
+
+		AND @oldrating <> @rating)
+
+	BEGIN
+		UPDATE listening_history
+		SET rating = @rating
+		WHERE alb_id = @albumid
+
+		RAISERROR(@updatedrating, 0, 1) WITH NOWAIT
+	END	
+
+-- Pokud to tam je, ale rating a datum jsou (for some reason) null, updatuju. Nemelo by se ale stat.
 
 	IF EXISTS
 			(SELECT alb_id 
@@ -67,6 +91,8 @@ BEGIN
 		RAISERROR(@updatedrating, 0, 1) WITH NOWAIT 
 	END
 
+-- Pokud to tam neni vubec, jednoduse vlozim rating, datum a favs.
+
 	IF NOT EXISTS
 			(SELECT alb_id
 			FROM listening_history
@@ -78,11 +104,23 @@ BEGIN
 		RAISERROR(@insertedstuff, 0, 1) WITH NOWAIT
 	END
 
+-- Jestli jsem neco zle zadal etc, tak to hodi error. Mozno dodelat ruzne druhy na zaklade typu chyby.
+
 	ELSE
 	
 	BEGIN
-		RAISERROR ('Nothing was added.', 0, 1)
-		RETURN
+		IF @oldrating = @rating
+
+		BEGIN
+			RAISERROR('Stary rating se rovna novemu.', 0, 1)
+		END
+
+		ELSE
+		
+		BEGIN
+			RAISERROR ('Nothing was added.', 0, 1)
+			RETURN
+		END
 	END
 END
 ;
